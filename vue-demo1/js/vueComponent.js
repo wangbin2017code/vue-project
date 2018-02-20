60a6874f852cc37daecc914370690f5695fca3ff
wangbin2017code <1329726333@qq.com>
2018-02-20 21:43:39 +0800

    update
    新增Vue自定义指令，slot,mixns...

--- vue-demo1/js/vueComponent.js
+++ vue-demo1/js/vueComponent.js
@@ -150,3 +150,131 @@ Vue.component('show',{
 new Vue({
     el:'#sonToParentConcatId'
 });
+
+// 组件间互相通信
+var Event = new Vue();
+Vue.component('zhangsan',{
+    template:
+    '<div>我说:<input @keyup="on_change" type="text" v-model="i_said"></div>',
+
+    data:function(){
+        return {
+            i_said:''
+        }
+    },
+    methods:{
+        on_change:function(){
+            Event.$emit('zhangsan_said_word',this.i_said);
+        }
+    }
+});
+
+Vue.component('lisi',{
+    template:'<div>zhangsan说:{{zhangsan_said}}</div>',
+    data:function(){
+        return {
+            zhangsan_said:''
+        }
+    },
+
+    mounted:function(){// 生命周期钩子
+        var me = this;
+        Event.$on('zhangsan_said_word',function(data){
+           me.zhangsan_said = data;
+        });
+    }
+});
+
+new Vue({
+    el:'#componentConcatId'
+});
+
+// 过滤器
+Vue.filter('currency',function(val,unit){
+    var val = val || 0;
+    var unit = unit || '元';
+    return val+unit;
+});
+
+Vue.filter('meter',function(val,unit){
+    val = val || 0;
+    unit = unit || 'm';
+    return (val/1000).toFixed(3)+unit;
+});
+
+new Vue({
+    el:'#vueFilterId',
+    data:{
+        goodPrice:20,
+        length:100,
+    }
+});
+
+// 自定义指令
+Vue.directive('pin',function(el,binding){
+    console.log(binding)
+    var val = binding.value;
+    var x = binding.modifiers;// 修饰符
+    var y = binding.arg;// 参数
+    console.log(y);
+    /*console.log('val:',val);
+    console.log('el:',el);*/
+    if(val){
+        el.style.color='yellow';
+    }else{
+        el.style.color = 'red'
+    }
+});
+new Vue({
+    el:'#selfDirectiveId',
+    data:{
+        card1_flag:true,
+        card2_flag:false,
+    }
+});
+
+//mixins混合（提取组件共通属性和方法）
+var base ={
+    methods:{
+        show:function(){
+            this.visible = true;
+        },
+        hide:function(){
+            this.visible = false;
+        }
+    },
+    data:function(){
+        return {
+            visible:false
+        }
+    }
+};
+Vue.component('tooltip',{
+    template:'<div><div @mouseenter="show" @mouseleave="hide">王斌  <span v-if="visible">你好</span></div></div>',
+    /*methods:{
+        show:function(){
+            this.visible = true;
+        },
+        hide:function(){
+            this.visible = false;
+        }
+    },
+    data:function(){
+        return {
+            visible:false
+        }
+    }*/
+    mixins:[base]
+});
+
+new Vue({
+    el:'#mixinsId'
+});
+
+Vue.component("panel",{
+    template:'#soltTemplateId',
+});
+new Vue({
+    el:'#slotId'
+});
+
